021a0c48348b562d753d36b7ee11f67af35b68f0
Molnar Csenge Anna <molnarcsengeanna@ktch.hu>
2023-04-18 14:08:24 +0200

    kedzés

---
+++ database/data.sql
@@ -0,0 +1,6 @@
+insert into employees(name, city, salary)
+values('Erős István', 'Szeged', 384),
+values('Piros Irén', 'Szeged', 384),
+values('Tilos Péter', 'Szolnok', 394),
+values('Alvó Katalin', 'Tata', 381),
+values('Dankó István', 'Szeged', 364);
